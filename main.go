c979712ee7a037130d5b456db9c3dfc11401c0e8
Ruchitareddy <57956470+ruchired@users.noreply.github.com>
2026-01-04 18:50:09 -0500

    Initial Go app to read JSON from S3

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type Person struct {
+	Name    string  `json:"name"`
+	Email   string  `json:"email"`
+	Phone   string  `json:"phone"`
+	Balance float64 `json:"balance"`
+}
+
+func main() {
+
+	bucket := "amazn-s3-demo-project-golang"
+	key := "sample2.json"
+
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("us-east-2"))
+
+	if err != nil {
+		log.Fatalf("unable to load AWS config: %v", err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	obj, err := client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+	})
+	if err != nil {
+		log.Fatalf("failed to get object: %v", err)
+	}
+	defer obj.Body.Close()
+
+	data, err := io.ReadAll(obj.Body)
+	if err != nil {
+		log.Fatalf("failed to read object body: %v", err)
+	}
+
+	// Try parsing as array first; if it fails, parse as single object.
+	var people []Person
+
+	if err := json.Unmarshal(data, &people); err != nil {
+		log.Fatalf("JSON must be an array of Person objects: %v", err)
+	}
+
+	for _, p := range people {
+		if p.Balance < 500 {
+			fmt.Println("LOW BALANCE:", p.Name, p.Balance)
+		} else {
+			fmt.Println("OK:", p.Name, p.Balance)
+		}
+	}
+
+}
